b41a31326c596dee118cee3895e4ee22d649488c
brandongon1 <48324169+brandongon1@users.noreply.github.com>
2019-05-30 08:48:40 -0400

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 var image= [];
 var names= [];
 var price= [];
+var totalprice;
 $(".itembutton").click(function(){
+    totalprice=0;
     var imageval = $(".imagein").val();
     var nameval = $(".namein").val();
     var priceval = parseInt($(".pricein").val());
-    price.forEach(function(){});
+   
     image.push(imageval);
     names.push(nameval);
     price.push(priceval);
     $(".masters").append("<div class='master'><img src="+imageval+"><p>"+nameval+"</p><p>$"+priceval+"</p></div");
-   var itemcart = names.length;
+     price.forEach(function(total){});
+    //totalprice = totalprice+parseInt(total);
+    var itemcart = names.length;
   $(".itemcart").text(itemcart);
   
 });
